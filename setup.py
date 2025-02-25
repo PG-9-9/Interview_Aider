3ea19131b61689fcf992db908040d4bf3b829a8a
PG-9-9 <vishaal.saravanan@fau.de>
2025-02-25 18:04:28 +0100

    Added Working Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Source Package for IntervieweQA',
+    version = '0.1',	
+    author = 'PG-9-9',
+    author_email='vishaals0507@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
